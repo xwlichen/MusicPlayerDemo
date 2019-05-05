879fc9e8001d6e4959921b5eb1a34ba83c36e566
lichen <1960003945@qq.com>
2019-05-05 21:59:22 +0800

    播放本地音乐

--- app/src/main/java/com/smart/musicplayerdemo/MainActivity.java
+++ app/src/main/java/com/smart/musicplayerdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
         MusicPlayer musicPlayer = findViewById(R.id.musicplayer);
 
-        String source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4";
+//        String source1 = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4";
+//        String source1="https://newst.bailemi.com/20190429/TMKQHTcFGfkJ8nQZAn2yCDHGSi7tYaHD.mp3";
+        String source1=Environment.getExternalStorageDirectory().getPath()+"/七里香.flac";
         musicPlayer.setUp(source1, true, "测试视频");
         musicPlayer.startPlayLogic();
 
